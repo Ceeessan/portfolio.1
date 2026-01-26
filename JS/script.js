7c276fdde9c84ee8ea7fad3fd3caf05932289dc2
Ceeessan <Cessan__94@hotmail.com>
2026-01-26 21:17:32 +0100

    Updated Styling and CV

--- JS/script.js
+++ JS/script.js
@@ -112,7 +112,7 @@ const toggleText = {
         languageToggler: "English",
         portfolioBy: "Portfolio av",
         webDeveloper: "Jag är Webbutvecklare...",
-        webDeveloperText: "med fokus på fullstack-utveckling för moderna och användarvänliga webbapplikationer.",
+        webDeveloperText: "med fokus på fullstack-utveckling och starkt intresse för webbanalys och datadriven utveckling!",
         aboutMe: "Om mig",
         cv: "Ladda ner CV",
         competence: "Kompetens",
@@ -120,6 +120,8 @@ const toggleText = {
         project1Open: "Se projektet",
         project2Open: "Se projektet",
         project3Open: "Se projektet",
+        learningAnalysisText1: "Under 2025 började jag arbeta med föreningen Munskänkarna, där jag fick i uppdrag att analysera trafiken på deras webbplats med verktygen Google Analytics 4 (GA4) och Matomo. Intresset för digital analys har växt och jag har fördjupat mina kunskaper genom att ta ett certifikat för GA4.",
+        learningAnalysisText2: "Just nu fördjupar jag mina kunskaper genom en YH-utbildning inom webbanalys, där jag arbetar med:",
         knowledgeText: "Jag har erfarenhet av att utveckla webbapplikationer från grunden, både med frontend och backend. Jag arbetar med moderna tekniker som HTML, CSS, JavaScript och TypeScript samt ramverk som Angular och React. På backend-sidan har jag använt Node.js och Express i kombination med databaser som MongoDB. Jag är van att arbeta med Git och GitHub.",
         contactMe: "Kontakta mig",
         updateHeader: "Vad gör jag nu?",
@@ -129,7 +131,7 @@ const toggleText = {
         languageToggler: "Swedish",
         portfolioBy: "Portfolio by",
         webDeveloper: "I am a Web Developer...",
-        webDeveloperText: "focusing on fullstack development of modern and user-friendly web applications.",
+        webDeveloperText: "focused on full-stack development, with a strong interest in web analytics and data-driven development!",
         aboutMe: "About Me",
         cv: "Download CV",
         competence: "Competence",
@@ -137,6 +139,8 @@ const toggleText = {
         project1Open: "See Project",
         project2Open: "See Project",
         project3Open: "See Project",
+        learningAnalysisText1: "In 2025, I started working with the association Munskänkarna, where I was tasked with analyzing their website traffic using Google Analytics 4 (GA4) and Matomo. My interest in digital analytics grew, and I further developed my skills by earning a GA4 certification.",
+        learningAnalysisText2: "I am currently expanding my expertise in web analytics through a vocational education program, focusing on:",
         knowledgeText: "I have experience developing web applications from scratch, both on the frontend and backend. I work with modern technologies such as HTML, CSS, JavaScript, and TypeScript, as well as frameworks like Angular and React. On the backend, I have used Node.js and Express in combination with databases like MongoDB. I’m comfortable working with Git and GitHub.",
         contactMe: "Contact me",
         updateHeader: "What am I doing now?",
@@ -155,7 +159,8 @@ const myProjectsText = document.getElementById('myProjectsText');
 const project1Open = document.getElementById('project1Open');
 const project2Open = document.getElementById('project2Open');
 const project3Open = document.getElementById('project3Open');
-
+const learningAnalysisText1 = document.getElementById('learningAnalysisText1');
+const learningAnalysisText2 = document.getElementById('learningAnalysisText2');
 const knowledgeText = document.getElementById('knowledgeText');
 const contactMe = document.getElementById('contactMe');
 const updateHeader = document.getElementById('nowUpdate');
@@ -179,6 +184,8 @@ function switchLanguage() {
     project1Open.textContent = toggleText[currentLanguage].project1Open;
     project2Open.textContent = toggleText[currentLanguage].project2Open;
     project3Open.textContent = toggleText[currentLanguage].project3Open;
+    learningAnalysisText1.textContent = toggleText[currentLanguage].learningAnalysisText1;
+    learningAnalysisText2.textContent = toggleText[currentLanguage].learningAnalysisText2;
     knowledgeText.textContent = toggleText[currentLanguage].knowledgeText;
     contactMe.textContent = toggleText[currentLanguage].contactMe;
     updateHeader.textContent = toggleText[currentLanguage].updateHeader;
